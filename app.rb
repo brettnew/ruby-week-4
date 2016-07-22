37d0a8040ea949bfafdefd358a830166f3d03142
Brett New <brett.new91@gmail.com>
2016-07-22 12:02:33 -0700

    add functionality to allow user to add shoe brands to stores via checkboxes.

--- app.rb
+++ app.rb
@@ -43,9 +43,25 @@ post('/brands') do
 end
 
 get('/stores/:id') do
-
+  @store = Store.find(params.fetch("id").to_i)
+  @brands = Brand.all
+  erb(:store)
 end
 
 get('/brands/:id') do
 
+end
+
+patch('/stores/:id') do
+  new_brand_ids = params.fetch('brand_id')
+  @store = Store.find(params.fetch('id').to_i)
+  brand_ids_array = []
+  @store.brands.each do |brand|
+    brand_ids_array.push(brand.id)
+  end
+  new_brand_ids.each() do |id|
+    brand_ids_array.push(id.to_i)
+  end
+  @store.update({:brand_ids => brand_ids_array})
+  redirect back
 end
